9ae3166213c60fc74e96039bf801ec034e4e381e
Isao Yagi <isao.yagi@gmail.com>
2006-08-10 18:11:32 +0000

    add some comments, minor SmacsFile changes

--- Smacs.class.php
+++ Smacs.class.php
@@ -7,23 +7,28 @@ class Smacs
 {
 	public static $keyprefix = '{';
 	public static $keysuffix = '}';
-	protected $out; ///! (string)
-	
+	protected $out; ///!@var (string) template data
+
 	public function __construct($s)
 	{
 		$this->set($s);
 	}
-	
+
 	public function set($s)
 	{
-		$this->out = $s; 
+		$this->out = $s;
 	}
-	
+
+	public function add($s)
+	{
+		$this->out .= $s;
+	}
+
 	public function mixIn($kv, $encode=0)
 	{
-		$this->out = $this->mixOut($kv, $this->out, $encode); 
+		$this->out = $this->mixOut($kv, $this->out, $encode);
 	}
-	
+
 	public function mixOut(&$kv, &$s, $encode=0)
 	{
 		$k = array_keys($kv);
@@ -45,7 +50,7 @@ class Smacs
 	{
 		return $this->out;
 	}
-	
+
 	public function __toString() {
 		return $this->out();
 	}
@@ -54,31 +59,52 @@ class Smacs
 
 class SmacsFile extends Smacs
 {
-	public static $tplext = '.html';
+	public static $tplext = '.tpl.html';
 	public static $phpext = '.php';
 
 	public function __construct($s='')
 	{
-	  if($s == '' or is_dir($s)) $s = $this->defaultfile($s);
-		$this->set(file_get_contents($s));
+		$this->load($s);
 	}
 
-	protected function defaultfile($s)
+	/**
+	 * Append a file to the template; if a path or empty string is supplied, use a
+	 * default filename.
+	 * @param $s (string) file, path, or empty string.
+	 */
+	public function load($s='')
 	{
-		if($s == '') {
-			$s = './';
-		} elseif(substr($s, -1) != DIRECTORY_SEPARATOR) {
-			$s.= DIRECTORY_SEPARATOR;
+	  if($s == '' or is_dir($s)) $s = $this->defaultFile($s);
+		if(is_file($s)) $this->add(file_get_contents($s));
+	}
+
+	/**
+	 * Gets the default template filename using an optional path parameter. If no
+	 * path is provided, use the path to the calling php script.
+	 * The default name is calculated taking the calling script filename, and
+	 * swapping extensions/suffixes; i.e. self::$phpext for self::$tplext.
+	 * @param $d (string) path to look for default filename
+	 * @return (string) a defualt path and filename
+	 */
+	protected function defaultFile($d)
+	{
+		if($d == '') {
+			$d = './';
+		} elseif(substr($d, -1) != DIRECTORY_SEPARATOR) {
+			$d.= DIRECTORY_SEPARATOR;
 		}
-		return $s.basename($_SERVER['SCRIPT_NAME'], self::$phpext).self::$tplext;
+		return $d.basename($_SERVER['SCRIPT_NAME'], self::$phpext).self::$tplext;
 	}
 
 }
 
 class SmacsBuffer extends Smacs
 {
+	public $oblevel;
+
 	public function __construct()
 	{
+		$this->oblevel = ob_get_level();
 		$files = func_get_args();
 		if(count($files)) {
 			foreach($files as $f) $this->bufferFile($f);
@@ -89,7 +115,7 @@ class SmacsBuffer extends Smacs
 
 	public function bufferStart()
 	{
-	  if(!ob_get_level()) ob_start();
+	  if($this->oblevel < ob_get_level()) ob_start();
 	}
 
 	public function bufferFile($f=null)
@@ -101,7 +127,7 @@ class SmacsBuffer extends Smacs
 			ob_end_clean();
 		}
 	}
-	
+
 	public function bufferIn()
 	{
 		$this->out .= ob_get_contents();
@@ -109,16 +135,16 @@ class SmacsBuffer extends Smacs
 
 	public function bufferEnd()
 	{
-		while(ob_get_level()) $this->out .= ob_end_clean();
+		while($this->oblevel < ob_get_level()) $this->out .= ob_end_clean();
 	}
 
 }
 
 class Slice extends Smacs
 {
-	protected $context; ///! (object)
-	protected $rgx;     ///! (string) regular expression
-	protected $mold;    ///! (string) sub template
+	protected $context; ///!@var (object)
+	protected $rgx;     ///!@var (string) regular expression
+	protected $mold;    ///!@var (string) sub template, stays clean
 
 	public function __construct(Smacs &$context, $beg, $end=null)
 	{
@@ -127,7 +153,7 @@ class Slice extends Smacs
 		$end = $end ? preg_quote($end) : $beg;
 		$this->rgx = "/$beg([\s\S]*?)$end/";
 		if(!preg_match($this->rgx, $this->context->out(), $m)) {
-			throw new Exception("slice pattern `$this->rgx` not found in template\n", E_USER_ERROR);
+			throw new Exception("slice pattern '$this->rgx' not found in template\n", E_USER_ERROR);
 		}
 		$this->mold = $m[1];
 		$this->out = '';
